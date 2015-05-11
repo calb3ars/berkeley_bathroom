d159e83fffed04d1dfb5c6c5dc282bd8fca4795e
calb3ars <andrewfjiang@gmail.com>
2015-05-11 14:44:17 -0700

    template inheritance (using tutorial naming convention)

--- app/views.py
+++ app/views.py
@@ -5,28 +5,28 @@ from app import app
 @app.route('/index') #map /index file
 def index():
 	user = {'nickname': 'Little Tinkler'} #fake user
-	reviews = [
+	posts = [
 		{
-			'user': {'nickname': 'Lost in Dwinelle'},
-			'review': 'I can\'t find a restroom in this d*mn place!'
+			'author': {'nickname': 'Lost in Dwinelle'},
+			'body': 'I can\'t find a restroom in this d*mn place!'
 		},
 		{
-			'user': {'nickname': 'Moaning Myrtle'},
-			'review': 'Watch out for the basilisk'
+			'author': {'nickname': 'Moaning Myrtle'},
+			'body': 'Watch out for the basilisk'
 		},
 		{
-			'user': {'nickname': 'John Doe'},
-			'review': 'I like a view when I pee'
+			'author': {'nickname': 'John Doe'},
+			'body': 'I like a view when I pee'
 		},
 		{
-			'user': {'nickname': 'This is the Wurster'},
-			'review': 'This is actually the best'
+			'author': {'nickname': 'This is the Wurster'},
+			'body': 'This is actually the best'
 		}
 	]
 	return render_template("index.html",
 				title='Let\'s Pee!',
 				user=user,
-				reviews=reviews)
+				posts=posts)
 
 #you can change the URL based on variables
 #@app.route('/user/<username>')
