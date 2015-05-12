ec600a9ee08aa35a9c2bf895c2daa859939f3157
calb3ars <andrewfjiang@gmail.com>
2015-05-11 18:05:27 -0700

    added OpenID providers

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template #function to load template from Flask framework
+from flask import render_template, flash, redirect #function to load template from Flask framework
 from app import app
 from .forms import LoginForm
 
@@ -32,9 +32,14 @@ def index():
 @app.route ('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
+	if form.validate_on_submit():
+			flash('Login requested for OpenID="%s", remember_me=%s' %
+				(form.openid.data, str(form.remember_me.data)))
+			return redirect('/index')
 	return render_template('login.html',
 							title='Sign In',
-							form=form)
+							form=form,
+							providers=app.config['OPENID_PROVIDERS'])
 
 #you can change the URL based on variables
 #@app.route('/user/<username>')
